b656103750d0445d3e8dcf506a17d6ade677b296
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-08-28 17:17:33 +0530

    Add files via upload

---
+++ Non-Repeating Element.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+    int firstNonRepeating(int arr[], int n) 
+    { 
+        // Complete the function
+        unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+        mp[arr[i]]++;
+  
+     // Traverse array again and return
+    // first element with count 1.
+    for (int i = 0; i < n; i++)
+        if (mp[arr[i]] == 1)
+            return arr[i];
+    return -1;
+        
+    } 
+  
+};
